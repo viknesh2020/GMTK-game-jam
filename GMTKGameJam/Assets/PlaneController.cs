359465ca41231e17ef04fa19af5b7b38b82bac3e
Allenvimal <36024568+Allenvimal@users.noreply.github.com>
2020-07-11 20:41:21 +0530

    Basic Asset_v0
    Basic asset

---
+++ GMTKGameJam/Assets/PlaneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    public Rigidbody objRigidbody;
+
+    public float rotaionSpeed;
+    public float sideForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        objRigidbody.AddForce(Vector3.left * Time.deltaTime * sideForce);
+        objRigidbody.AddRelativeTorque(Vector3.forward*Time.deltaTime*rotaionSpeed);
+    }
+}
